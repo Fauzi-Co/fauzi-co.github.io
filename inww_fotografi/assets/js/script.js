48b26e3106965159a403d96a9b01e95039aaf21b
Fauzi-Co <eexxeezy@gmail.com>
2021-06-25 21:23:34 +0800

    add a animate in jumbotron

--- inww_fotografi/assets/js/script.js
+++ inww_fotografi/assets/js/script.js
@@ -1,7 +1,15 @@
 const navDrop = document.querySelector(".navbar-drop");
+// const navLinkDrop = navDrop.querySelector(".collapse");
+// const btnLinkDrop = navDrop.querySelector("button.navbar-toggler");
+const jumbotronImg = document.querySelector(".jumbotron .pic img");
+const jumbotronTxt = document.querySelector(".jumbotron .text");
+
+window.addEventListener("load", () => {
+  jumbotronImg.classList.add("animate");
+  jumbotronTxt.classList.add("animate");
+});
 
 document.addEventListener("scroll", () => {
-  console.log(window.scrollY);
   if (window.scrollY > 30) {
     navDrop.classList.add("shadow");
   } else if (window.scrollY < 30) {
