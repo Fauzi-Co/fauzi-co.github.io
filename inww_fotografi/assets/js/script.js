7e2b041dec3c8616c3b330e160e557d4a63af3d9
Fauzi-Co <eexxeezy@gmail.com>
2021-06-26 10:45:08 +0800

    add animasi to service'

--- inww_fotografi/assets/js/script.js
+++ inww_fotografi/assets/js/script.js
@@ -1,6 +1,11 @@
+// navbar e
 const navDrop = document.querySelector(".navbar-drop");
+
+// jumbotron e
 const jumbotronImg = document.querySelector(".jumbotron .pic img");
 const jumbotronTxt = document.querySelector(".jumbotron .text");
+
+// galery e
 const galeryH3 = document.querySelector(".galery h3");
 const col_1_Img1 = document.querySelector(".col-1 .img-1");
 const col_1_Img2 = document.querySelector(".col-1 .img-2");
@@ -8,6 +13,12 @@ const col_2_Img1 = document.querySelector(".col-2 .img-1");
 const col_2_Img2 = document.querySelector(".col-2 .img-2");
 const col_3_Img1 = document.querySelector(".col-3 .img-1");
 const col_3_Img2 = document.querySelector(".col-3 .img-2");
+
+// service e
+const servicePic = document.querySelector("section.service .pic");
+const service_1 = document.querySelector("section.service .service-1");
+const service_2 = document.querySelector("section.service .service-2");
+const service_3 = document.querySelector("section.service .service-3");
 
 window.addEventListener("load", () => {
   jumbotronImg.classList.add("animate");
@@ -29,13 +40,12 @@ document.addEventListener("scroll", () => {
     col_3_Img2.classList.add("animate");
     col_2_Img2.classList.add("animate");
     col_1_Img2.classList.add("animate");
-  } else if (window.scrollY < 250) {
-    galeryH3.classList.remove("animate");
-    col_1_Img1.classList.remove("animate");
-    col_2_Img1.classList.remove("animate");
-    col_3_Img1.classList.remove("animate");
-    col_3_Img2.classList.remove("animate");
-    col_2_Img2.classList.remove("animate");
-    col_1_Img2.classList.remove("animate");
+  }
+
+  if (window.scrollY > 1200) {
+    servicePic.classList.add("animate");
+    service_1.classList.add("animate");
+    service_2.classList.add("animate");
+    service_3.classList.add("animate");
   }
 });
